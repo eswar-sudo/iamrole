33aed17663ee90712f06874210dbb7096f6081d9
eswar-sudo <eswarkumar501@gmail.com>
2025-09-10 13:41:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_iam_role" "example_role" {
+  name = var.name
+  assume_role_policy = jsonencode(var.assume_role_policy)
+}
+
+# Attach AWS managed policies (optional)
+resource "aws_iam_role_policy_attachment" "this" {
+  for_each   = toset(var.managed_policy_arns)
+  role       = aws_iam_role.this.name
+  policy_arn = each.value
+}
